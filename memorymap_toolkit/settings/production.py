ac05c5aa43f2f34c4cf7e4a0444f361fb7ad5e03
Duncan Hay <duncan@Duncans-iMac.local>
2020-11-30 19:32:59 +0000

    logging

--- memorymap_toolkit/settings/production.py
+++ memorymap_toolkit/settings/production.py
@@ -3,3 +3,22 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR.child('logs') + 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
