64cc9ca9b66a8f7b29e2a460f83cd86979c50be6
Duncan Hay <duncan@Duncans-iMac.local>
2021-02-02 12:46:32 +0000

    mapbox version in admin

--- mmt_map/widgets.py
+++ mmt_map/widgets.py
@@ -1,4 +1,8 @@
+# Django core
 from django.contrib.gis import forms
+
+# Third Party Django apps
+from constance import config
 
 class MapBoxGLWidget(forms.BaseGeometryWidget):
 	template_name = 'mmt_map/forms/widgets/mapboxgl_widget.html'
@@ -10,6 +14,6 @@ class MapBoxGLWidget(forms.BaseGeometryWidget):
 
 	class Media:
 		css = {
-			'all': ('https://api.tiles.mapbox.com/mapbox-gl-js/v1.6.1/mapbox-gl.css', 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.0.9/mapbox-gl-draw.css')
+			'all': ('https://api.tiles.mapbox.com/mapbox-gl-js/' + config.MAPBOX_VERSION + '/mapbox-gl.css', 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.css')
 		}
-		js= ('https://api.tiles.mapbox.com/mapbox-gl-js/v1.6.1/mapbox-gl.js', 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.0.9/mapbox-gl-draw.js', 'https://cdn.jsdelivr.net/npm/@turf/turf@5/turf.min.js')
+		js = ('https://api.tiles.mapbox.com/mapbox-gl-js/' + config.MAPBOX_VERSION + '/mapbox-gl.js', 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.js', 'https://cdn.jsdelivr.net/npm/@turf/turf@5/turf.min.js')
